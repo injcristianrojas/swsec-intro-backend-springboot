b23a555c21d086298addc103ad46ad2133598845
Cristián Rojas <injcristianrojas@gmail.com>
2024-05-18 14:00:01 -0400

    Basic API

---
+++ src/main/java/cl/injcristianrojas/controllers/v1/UserController.java
@@ -0,0 +1,16 @@
+package cl.injcristianrojas.controllers.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+  @GetMapping("/users")
+  public String retrieveAllUsers() {
+    return "holi";
+  }
+
+}
