06bb34b7dd93761661515a6414d6356c1a0e9593
Cristián Rojas <injcristianrojas@gmail.com>
2024-05-18 13:50:47 -0400

    Package change

--- src/main/java/cl/injcristianrojas/ServletInitializer.java
+++ src/main/java/cl/injcristianrojas/ServletInitializer.java
@@ -1,4 +1,4 @@
-package cl.injcristianrojas.swsecintro;
+package cl.injcristianrojas;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- src/main/java/cl/injcristianrojas/SwsecintroApplication.java
+++ src/main/java/cl/injcristianrojas/SwsecintroApplication.java
@@ -1,4 +1,4 @@
-package cl.injcristianrojas.swsecintro;
+package cl.injcristianrojas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
