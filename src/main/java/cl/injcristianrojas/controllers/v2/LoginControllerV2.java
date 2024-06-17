54934aa6b5a7e166badeaa8e2fe30a2381eb6b5c
Cristián Rojas <injcristianrojas@gmail.com>
2024-06-17 18:10:50 -0400

    Better auth endpoint

--- src/main/java/cl/injcristianrojas/controllers/v2/LoginControllerV2.java
+++ src/main/java/cl/injcristianrojas/controllers/v2/LoginControllerV2.java
@@ -1,13 +1,16 @@
 package cl.injcristianrojas.controllers.v2;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,15 +30,17 @@ public class LoginControllerV2 {
   private AuthenticationManager authenticationManager;
 
   @PostMapping("/login")
-  public Map<String, Object> authenticateAndGetToken(@RequestBody UserJPA authRequest) {
-    Authentication authentication = authenticationManager
-        .authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
-    if (authentication.isAuthenticated()) {
+  public ResponseEntity<Map<String, Object>> authenticateAndGetToken(@RequestBody UserJPA authRequest) {
+    try {
+      Authentication authentication = authenticationManager
+          .authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
       Map<String, Object> map = new HashMap<>();
       map.put("token", jwtService.generateToken(authentication));
-      return map;
-    } else
-      throw new UsernameNotFoundException("invalid user request!");
+      return ResponseEntity.ok(map);
+    } catch (BadCredentialsException e) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+          .body(Collections.singletonMap("message", "Invalid username or password"));
+    }
   }
 
 }
