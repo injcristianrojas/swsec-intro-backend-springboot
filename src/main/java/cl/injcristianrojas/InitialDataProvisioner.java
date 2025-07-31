d9b2e79f42b89a84086b7d291a9ea5d2c56e89cc
Cristián Rojas <injcristianrojas@gmail.com>
2025-07-31 15:30:06 -0400

    Format InitialDataProvider

--- src/main/java/cl/injcristianrojas/InitialDataProvisioner.java
+++ src/main/java/cl/injcristianrojas/InitialDataProvisioner.java
@@ -1,11 +1,5 @@
 package cl.injcristianrojas;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-
 import cl.injcristianrojas.data.model.Message;
 import cl.injcristianrojas.data.model.Role;
 import cl.injcristianrojas.data.model.UserJPA;
@@ -13,63 +7,72 @@ import cl.injcristianrojas.data.repositories.MessageRepository;
 import cl.injcristianrojas.data.repositories.RoleRepository;
 import cl.injcristianrojas.data.repositories.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 @Component
-public class InitialDataProvisioner implements ApplicationListener<ContextRefreshedEvent> {
+public class InitialDataProvisioner
+    implements ApplicationListener<ContextRefreshedEvent> {
 
-  private boolean alreadySetup = false;
-  private static final String ROLE_ADMIN = "ROLE_ADMIN";
-	private static final String ROLE_USER = "ROLE_USER";
+    private boolean alreadySetup = false;
+    private static final String ROLE_ADMIN = "ROLE_ADMIN";
+    private static final String ROLE_USER = "ROLE_USER";
 
-  @Autowired
-	private MessageRepository messageRepository;
-  @Autowired
-	private RoleRepository roleRepository;
-  @Autowired
-	private PasswordEncoder passwordEncoder;
-  @Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private MessageRepository messageRepository;
 
-  @Override
-  public void onApplicationEvent(ContextRefreshedEvent event) {
-    if (alreadySetup)
-			return;
-    createRoleIfNotFound(ROLE_ADMIN);
-		createRoleIfNotFound(ROLE_USER);
+    @Autowired
+    private RoleRepository roleRepository;
 
-    createUser("zorzal", "fio", ROLE_USER);
-    createUser("admin", "123", ROLE_ADMIN);
-    createUser("chincol", "fiofio", ROLE_USER);
-    createUser("tiuque", "pah", ROLE_USER);
-    createUser("loica", "roji", ROLE_USER);
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    createPost("Bienvenidos a Fans de las Aves Chilenas. Soy el administrador.");
-  }
+    @Autowired
+    private UserRepository userRepository;
 
-  private void createUser(String username, String password, String rolename) {
-    UserJPA user = new UserJPA();
-		user.setUsername(username);
-		user.setPassword(passwordEncoder.encode(password));
-		user.setRole(roleRepository.findByRolename(rolename));
-		userRepository.save(user);
-		alreadySetup = true;
-  }
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (alreadySetup) return;
+        createRoleIfNotFound(ROLE_ADMIN);
+        createRoleIfNotFound(ROLE_USER);
 
-  @Transactional
-	private Role createRoleIfNotFound(String name) {
-		Role role = roleRepository.findByRolename(name);
-		if (role == null) {
-			role = new Role();
-			role.setName(name);
-			roleRepository.save(role);
-		}
-		return role;
-	}
+        createUser("zorzal", "fio", ROLE_USER);
+        createUser("admin", "123", ROLE_ADMIN);
+        createUser("chincol", "fiofio", ROLE_USER);
+        createUser("tiuque", "pah", ROLE_USER);
+        createUser("loica", "roji", ROLE_USER);
 
-  private void createPost(String message) {
-		Message post = new Message();
-		post.setMessage(message);
-		messageRepository.save(post);
-	}
+        createPost(
+            "Bienvenidos a Fans de las Aves Chilenas. Soy el administrador."
+        );
+    }
 
+    private void createUser(String username, String password, String rolename) {
+        UserJPA user = new UserJPA();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRole(roleRepository.findByRolename(rolename));
+        userRepository.save(user);
+        alreadySetup = true;
+    }
+
+    @Transactional
+    private Role createRoleIfNotFound(String name) {
+        Role role = roleRepository.findByRolename(name);
+        if (role == null) {
+            role = new Role();
+            role.setName(name);
+            roleRepository.save(role);
+        }
+        return role;
+    }
+
+    private void createPost(String message) {
+        Message post = new Message();
+        post.setMessage(message);
+        messageRepository.save(post);
+    }
 }
